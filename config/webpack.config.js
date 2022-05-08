0c0b7ddffd3fc5ff015c1a2745c5172b04042dd5
LuoHao <hluo5206@gmail.com>
2022-05-08 19:08:51 +0800

    build

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,3 @@
-import path from 'path';
-
 function chainWebpack(memo) {
   // memo.resolve.alias.set('antd', path.resolve('@/libs/ant-design/index.js'));
   // memo.resolve.alias.set('moment', path.resolve('@/libs/moment/moment.js'));
